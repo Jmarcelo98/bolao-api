80cc7e76b9dbbb1902afc73798c46c36d611726b
João Marcelo <joaomarcelo588@gmail.com>
2022-11-15 02:00:09 -0300

    Projeto criado

---
+++ src/main/java/br/com/estudos/bolaoapi/BolaoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.estudos.bolaoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolaoApiApplication.class, args);
+	}
+
+}
